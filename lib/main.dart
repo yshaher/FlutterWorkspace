5b68ea00e1dec626c5faa25cf210a9bda918d53c
yshaher <hsretra974@gmail.com>
2022-05-02 00:34:46 +0500

    First Application that include english words library it generate english and add into the and also saved button add in this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,29 +16,58 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello',
-      home: HomePage(),
-    );
+        title: 'Hello',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: Center(child: RandomWords()),
+        ));
   }
 }
 
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+class RandomWords extends StatefulWidget {
+  RandomWords({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<RandomWords> createState() => _RandomWordsState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _RandomWordsState extends State<RandomWords> {
+  final suggestions = <WordPair>[];
+  final saved = <WordPair>[];
+  final biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello'),
-      ),
-      body: Container(
-        child: Text('Hello World'),
-      ),
-    );
+    return ListView.builder(itemBuilder: (context, i) {
+      Padding:
+      const EdgeInsets.all(16.0);
+      if (i.isOdd) return const Divider();
+      final index = i ~/ 2;
+      if (index >= suggestions.length) {
+        suggestions.addAll(generateWordPairs().take(10));
+      }
+      final alreadySaved = saved.contains(suggestions[index]);
+      return ListTile(
+        title: Text(
+          suggestions[index].asPascalCase,
+          style: biggerFont,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+          semanticLabel: alreadySaved ? 'Remove from saved' : 'saved',
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              saved.remove(suggestions[index]);
+            } else {
+              saved.add(suggestions[index]);
+            }
+          });
+        },
+      );
+    });
   }
 }
